07be81e1c26ba020667638d3ae007a81eeb877ab
Tony Tamplin <tony.t@franq.co>
2015-06-27 18:07:59 +1000

    add some information to the user profile

--- client/templates/users/userProfile.js
+++ client/templates/users/userProfile.js
@@ -1,17 +1,28 @@
 Template.userProfile.helpers({
+  hasClasses: function() {
+    return this.user() && this.user().classes;
+  },
+
+  classes: function() {
+    return Object.keys(this.user().classes)
+      .map(function(className) {
+        return {className: className}
+      });
+  }
 });
 
 Template.userProfile.events({
   //add your events here
 });
 
-Template.userProfile.onCreated(function () {});
+Template.userProfile.onCreated(function() {
+});
 
-Template.userProfile.onRendered(function () {
+Template.userProfile.onRendered(function() {
   //add your statement here
 });
 
-Template.userProfile.onDestroyed(function () {
+Template.userProfile.onDestroyed(function() {
   //add your statement here
 });
 
