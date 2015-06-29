f9af5b8fade56f438f4d10dd079de0119ee5936d
Tony Tamplin <tony.t@franq.co>
2015-06-29 16:46:31 +1000

    Resolve the user once instead of every time we need it

--- both/routes/user.js
+++ both/routes/user.js
@@ -7,10 +7,12 @@ Router.map(function () {
       this.next();
     }
   });
+
   this.route('user/search', {
     path: '/user/search',
     template: 'userSearch'
   });
+
   this.route('user/:id', {
     path: '/user/:id',
     template: 'userProfile',
@@ -25,17 +27,29 @@ Router.map(function () {
       }
     }
   });
+
   this.route('user/:id/edit', {
     path: '/user/:id/edit',
     template: 'userEdit',
     waitOn: function () {
       Meteor.subscribe('users');
     },
-    data: {
-      user: function () {
-        return User.findOne({
-          _id: Router.current().params.id
-        });
+    data: function() {
+      return {
+        user: User.findOne({_id: Router.current().params.id})
+      }
+    }
+  });
+
+  this.route('user/:id/settings', {
+    path: '/user/:id/settings',
+    template: 'userSettings',
+    waitOn: function() {
+      Meteor.subscribe('users');
+    },
+    data: function() {
+      return {
+        user: User.findOne({_id: Router.current().params.id})
       }
     }
   });
